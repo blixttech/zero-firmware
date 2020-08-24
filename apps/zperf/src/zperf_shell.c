f121e12a10a9aa8762020f63aa4036e71f70dc05
Kasun Hewage <kasun@blixt.tech>
2020-08-24 19:04:17 +0200

    Adding DHCP client

--- apps/zperf/src/zperf_shell.c
+++ apps/zperf/src/zperf_shell.c
@@ -1205,3 +1205,56 @@ SHELL_STATIC_SUBCMD_SET_CREATE(zperf_commands,
 );
 
 SHELL_CMD_REGISTER(zperf, &zperf_commands, "Zperf commands", NULL);
+
+
+static struct net_mgmt_event_callback mgmt_cb;
+
+static void handler(struct net_mgmt_event_callback *cb,
+            u32_t mgmt_event,
+            struct net_if *iface)
+{
+    int i = 0;
+
+    if (mgmt_event != NET_EVENT_IPV4_ADDR_ADD) {
+        return;
+    }
+
+    for (i = 0; i < NET_IF_MAX_IPV4_ADDR; i++) {
+        char buf[NET_IPV4_ADDR_LEN];
+
+        if (iface->config.ip.ipv4->unicast[i].addr_type !=
+                            NET_ADDR_DHCP) {
+            continue;
+        }
+
+        LOG_INF("Your address: %s",
+            log_strdup(net_addr_ntop(AF_INET,
+                &iface->config.ip.ipv4->unicast[i].address.in_addr,
+                          buf, sizeof(buf))));
+        LOG_INF("Lease time: %u seconds",
+             iface->config.dhcpv4.lease_time);
+        LOG_INF("Subnet: %s",
+            log_strdup(net_addr_ntop(AF_INET,
+                       &iface->config.ip.ipv4->netmask,
+                       buf, sizeof(buf))));
+        LOG_INF("Router: %s",
+            log_strdup(net_addr_ntop(AF_INET,
+                         &iface->config.ip.ipv4->gw,
+                         buf, sizeof(buf))));
+    }
+}
+
+void main(void)
+{
+    struct net_if *iface;
+
+    LOG_INF("Run dhcpv4 client");
+
+    net_mgmt_init_event_callback(&mgmt_cb, handler,
+                     NET_EVENT_IPV4_ADDR_ADD);
+    net_mgmt_add_event_callback(&mgmt_cb);
+
+    iface = net_if_get_default();
+
+    net_dhcpv4_start(iface);
+}
