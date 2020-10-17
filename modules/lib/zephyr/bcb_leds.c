5a7e13a06b8d2a34a587af0ec780b440fe3bad47
Kasun Hewage <kasun@blixt.tech>
2020-10-17 16:40:15 +0200

    Implemented LED toggling and other changes.

--- modules/lib/zephyr/bcb_leds.c
+++ modules/lib/zephyr/bcb_leds.c
@@ -9,7 +9,7 @@ LOG_MODULE_REGISTER(bcb_leds);
 
 #define BCB_GPIO_DEV(pin_name)      (bcb_leds_data.dev_##pin_name)
 
-#define BCB_INIT_GPIO_PIN(dt_nlabel, pin_name, pin_flags)                                               \
+#define BCB_GPIO_PIN_INIT(dt_nlabel, pin_name, pin_flags)                                               \
     do {                                                                                                \
         BCB_GPIO_DEV(pin_name) = device_get_binding(DT_LABEL(DT_PHANDLE_BY_NAME(DT_NODELABEL(dt_nlabel),\
                                                                 gpios, pin_name)));                     \
@@ -22,13 +22,17 @@ LOG_MODULE_REGISTER(bcb_leds);
                             (pin_flags));                                                               \
     } while(0)                                                                              
 
-#define BCB_SET_GPIO_PIN(dt_nlabel, pin_name, value)                                \
+#define BCB_GPIO_PIN_SET(dt_nlabel, pin_name, value)                                \
     gpio_pin_set_raw(BCB_GPIO_DEV(pin_name),                                        \
                     DT_PHA_BY_NAME(DT_NODELABEL(dt_nlabel), gpios, pin_name, pin),  \
                     (value))                     
 
-#define BCB_GET_GPIO_PIN(dt_nlabel, pin_name)                                       \
+#define BCB_GPIO_PIN_GET(dt_nlabel, pin_name)                                       \
     gpio_pin_get_raw(BCB_GPIO_DEV(pin_name),                                        \
+                    DT_PHA_BY_NAME(DT_NODELABEL(dt_nlabel), gpios, pin_name, pin))
+
+#define BCB_GPIO_PIN_TOGGLE(dt_nlabel, pin_name)                                    \
+    gpio_pin_toggle(BCB_GPIO_DEV(pin_name),                                         \
                     DT_PHA_BY_NAME(DT_NODELABEL(dt_nlabel), gpios, pin_name, pin))
 
 struct bcb_leds_data {
@@ -44,37 +48,42 @@ static struct bcb_leds_data bcb_leds_data = {
 void bcb_leds_on(int leds)
 {
     if (leds & BCB_LEDS_RED) {
-        BCB_SET_GPIO_PIN(user_iface, led_red, 0);
+        BCB_GPIO_PIN_SET(user_iface, led_red, 0);
     }
 
     if (leds & BCB_LEDS_GREEN) {
-        BCB_SET_GPIO_PIN(user_iface, led_green, 0);
+        BCB_GPIO_PIN_SET(user_iface, led_green, 0);
     }
 }
 
 void bcb_leds_off(int leds)
 {
     if (leds & BCB_LEDS_RED) {
-        BCB_SET_GPIO_PIN(user_iface, led_red, 1);
+        BCB_GPIO_PIN_SET(user_iface, led_red, 1);
     }
 
     if (leds & BCB_LEDS_GREEN) {
-        BCB_SET_GPIO_PIN(user_iface, led_green, 1);
+        BCB_GPIO_PIN_SET(user_iface, led_green, 1);
     }
 }
 
 void bcb_leds_toggle(int leds)
 {
+    if (leds & BCB_LEDS_RED) {
+        BCB_GPIO_PIN_TOGGLE(user_iface, led_red);
+    }
 
+    if (leds & BCB_LEDS_GREEN) {
+        BCB_GPIO_PIN_TOGGLE(user_iface, led_green);
+    }
 }
 
 static int bcb_leds_init()
 {
-
-    BCB_INIT_GPIO_PIN(user_iface, led_red, (GPIO_ACTIVE_HIGH | GPIO_OUTPUT_ACTIVE));
-    BCB_SET_GPIO_PIN(user_iface, led_red, 1);
-    BCB_INIT_GPIO_PIN(user_iface, led_green, (GPIO_ACTIVE_HIGH | GPIO_OUTPUT_ACTIVE));
-    BCB_SET_GPIO_PIN(user_iface, led_green, 1);
+    BCB_GPIO_PIN_INIT(user_iface, led_red, (GPIO_ACTIVE_HIGH | GPIO_OUTPUT_ACTIVE));
+    BCB_GPIO_PIN_SET(user_iface, led_red, 1);
+    BCB_GPIO_PIN_INIT(user_iface, led_green, (GPIO_ACTIVE_HIGH | GPIO_OUTPUT_ACTIVE));
+    BCB_GPIO_PIN_SET(user_iface, led_green, 1);
 
     return 0;
 }
