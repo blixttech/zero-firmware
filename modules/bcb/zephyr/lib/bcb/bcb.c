c5d36ca4edc3871069a5da0c15a2897cbfacc03b
Kasun Hewage <kasun.ch@gmail.com>
2021-02-17 12:06:14 +0100

    bcb: Moved actual open/close operations into the system workqueue
    Moving actual open/close operations into the system workqueue resolves
    state saving issues.
    
    Fixes #10
    
    Signed-off-by: Kasun Hewage <kasun.ch@gmail.com>

--- modules/bcb/zephyr/lib/bcb/bcb.c
+++ modules/bcb/zephyr/lib/bcb/bcb.c
@@ -3,31 +3,32 @@
 #include "bcb_msmnt.h"
 #include "bcb_trip_curve.h"
 #include <errno.h>
+#include <kernel.h>
 
 #define LOG_LEVEL CONFIG_BCB_LOG_LEVEL
 #include <logging/log.h>
 LOG_MODULE_REGISTER(bcb);
 
-enum bcb_state { BCB_STATE_NONE = 0x00, BCB_STATE_ON = 0x01, BCB_STATE_OFF = 0x02 };
-
 typedef struct __attribute__((packed)) bcb_config_data {
-	uint8_t state;
+	bool is_open;
 } bcb_config_data_t;
 
 struct bcb_data {
 	const struct bcb_trip_curve *trip_curve;
 	sys_slist_t callback_list;
 	bcb_config_data_t config;
+	struct k_work bcb_work;
 };
 
 static struct bcb_data bcb_data;
 
 static inline void load_default_config(void)
 {
-	bcb_data.config.state = BCB_STATE_ON;
+	bcb_data.config.is_open = false;
 }
 
-static void trip_curve_callback(const struct bcb_trip_curve *curve, bcb_trip_cause_t type, uint8_t limit)
+static void trip_curve_callback(const struct bcb_trip_curve *curve, bcb_trip_cause_t type,
+				uint8_t limit)
 {
 	sys_snode_t *node;
 	SYS_SLIST_FOR_EACH_NODE (&bcb_data.callback_list, node) {
@@ -38,64 +39,74 @@ static void trip_curve_callback(const struct bcb_trip_curve *curve, bcb_trip_cau
 	}
 }
 
+static void bcb_work(struct k_work *work)
+{
+	int r;
+
+	if (bcb_data.config.is_open) {
+		r = bcb_data.trip_curve->open();
+	} else {
+		r = bcb_data.trip_curve->close();
+	}
+
+	if (r) {
+		LOG_WRN("cannot perform operation: %d", r);
+	}
+
+	r = bcb_config_store(CONFIG_BCB_LIB_PERSISTENT_CONFIG_OFFSET_BCB,
+			     (uint8_t *)&bcb_data.config, sizeof(bcb_data.config));
+
+	if (r) {
+		LOG_WRN("cannot save configuration: %d", r);
+	}
+}
+
 int bcb_init()
 {
 	int r;
 
 	memset(&bcb_data, 0, sizeof(bcb_data));
 	sys_slist_init(&bcb_data.callback_list);
+	k_work_init(&bcb_data.bcb_work, bcb_work);
 
 	r = bcb_config_load(CONFIG_BCB_LIB_PERSISTENT_CONFIG_OFFSET_BCB,
 			    (uint8_t *)&bcb_data.config, sizeof(bcb_data.config));
 	if (r) {
 		load_default_config();
 	}
+
 	return 0;
 }
 
 int bcb_close(void)
 {
-	int r;
-
 	if (!bcb_data.trip_curve) {
 		LOG_ERR("Trip cruve not set");
 		return -EINVAL;
 	}
 
-	bcb_data.config.state = BCB_STATE_ON;
-	r = bcb_config_store(CONFIG_BCB_LIB_PERSISTENT_CONFIG_OFFSET_BCB,
-			     (uint8_t *)&bcb_data.config, sizeof(bcb_data.config));
+	bcb_data.config.is_open = false;
+	k_work_submit(&bcb_data.bcb_work);
 
-	if (r) {
-		LOG_WRN("Cannot save configuration: %d", r);
-	}
-
-	return bcb_data.trip_curve->close();
+	return 0;
 }
 
 int bcb_open(void)
 {
-	int r;
-
 	if (!bcb_data.trip_curve) {
 		LOG_ERR("Trip cruve not set");
 		return -EINVAL;
 	}
 
-	bcb_data.config.state = BCB_STATE_OFF;
-	r = bcb_config_store(CONFIG_BCB_LIB_PERSISTENT_CONFIG_OFFSET_BCB,
-			     (uint8_t *)&bcb_data.config, sizeof(bcb_data.config));
+	bcb_data.config.is_open = true;
+	k_work_submit(&bcb_data.bcb_work);
 
-	if (r) {
-		LOG_WRN("Cannot save configuration: %d", r);
-	}
-
-	return bcb_data.trip_curve->open();
+	return 0;
 }
 
 bool bcb_is_closed()
 {
-	return bcb_data.config.state == BCB_STATE_ON;
+	return !bcb_data.config.is_open;
 }
 
 int bcb_set_trip_curve(const struct bcb_trip_curve *curve)
@@ -134,7 +145,6 @@ int bcb_add_trip_callback(struct bcb_trip_callback *callback)
 	if (!callback || !callback->handler) {
 		return -ENOTSUP;
 	}
-
 
 	sys_slist_append(&bcb_data.callback_list, &callback->node);
 
